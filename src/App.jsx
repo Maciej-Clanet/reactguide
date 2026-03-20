1afd8da1dd5441bcec8589a699bbf6f765023782
Maciej-Clanet <43215153+Maciej-Clanet@users.noreply.github.com>
2026-03-20 22:40:40 +0000

    Classroom starting point done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import './App.css'
-import Header from './components/Header/Header'
-import HeroBanner from './components/HeroBanner/HeroBanner'
+import "./App.css";
+import Header from "./components/Header/Header";
+import HeroBanner from "./components/HeroBanner/HeroBanner";
 
 function App() {
-
   return (
     <>
       <Header />
       <main className="main-content">
         <HeroBanner />
-        <div className="all-sections">
-
-
-
-        </div>
+        <div className="all-sections"></div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
