3d8c53f311d910afe138f647b8ad1635a1769961
Maciej-Clanet <maciejclanet@gmail.com>
2026-03-20 09:59:38 +0000

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header/Header'
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main class="main-content">
+        stuff
+      </main>
+    </>
+  )
+}
+
+export default App
